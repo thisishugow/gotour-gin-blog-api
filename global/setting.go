f3d6b142e0a3252600ede9dc56b31cbe29f43df6
thisishugow <59921505+thisishugow@users.noreply.github.com>
2023-10-31 10:07:35 +0800

    Add vp config

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "yuwang.idv/go-tour/blog-api/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.ServerSettingS
+)
